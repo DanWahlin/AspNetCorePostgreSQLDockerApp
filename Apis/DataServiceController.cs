1e765d985fdeb4291e0520a1be05410bab3f6129
Dan Wahlin <dwahlin@xmlforasp.net>
2016-09-04 17:10:07 -0700

    Update typings version, fix spacing in systemjs config

--- Apis/DataServiceController.cs
+++ Apis/DataServiceController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCorePostgreSQLDockerApp.Models;
 using AspNetCorePostgreSQLDockerApp.Repository;
+using Microsoft.AspNetCore.Http;
 
 namespace AspNetCorePostgreSQLDockerApp.Apis
 {
@@ -19,43 +20,99 @@ namespace AspNetCorePostgreSQLDockerApp.Apis
 
         // GET api/dataservice/customers
         [HttpGet("customers")]
-        public async Task<List<Customer>> Customers()
+        public async Task<ActionResult> Customers()
         {
-            return await _repo.GetCustomersAsync();
+            var customers = await _repo.GetCustomersAsync();
+            if (customers == null) {
+              return NotFound();
+            }
+            return Ok(customers);
         }
 
         // GET api/dataservice/customers/5
         [HttpGet("customers/{id}")]
-        public async Task<Customer> Customers(int id)
+        public async Task<ActionResult> Customers(int id)
         {
-            return await _repo.GetCustomerAsync(id);
+            var customer = await _repo.GetCustomerAsync(id);
+            if (customer == null) {
+              return NotFound();
+            }
+            return Ok(customer);
         }
 
         // POST api/values
         [HttpPost("customers")]
-        public async Task<Customer> PostCustomer([FromBody]Customer customer)
+        public async Task<ActionResult> PostCustomer([FromBody]Customer customer)
         {
-          return await _repo.InsertCustomerAsync(customer);
+          if (!ModelState.IsValid) {
+            return BadRequest(this.ModelState);
+          }
+
+          var newCustomer = await _repo.InsertCustomerAsync(customer);
+          if (newCustomer == null) {
+            return BadRequest("Unable to insert customer");
+          }
+          var uri = Request.ToUri().ToString() + "/" + newCustomer.Id.ToString();
+          return Created(uri, newCustomer);
         }
 
         // PUT api/dataservice/customers/5
         [HttpPut("customers/{id}")]
-        public async Task<bool> PutCustomer(int id, [FromBody]Customer customer)
+        public async Task<ActionResult> PutCustomer(int id, [FromBody]Customer customer)
         {
-          return await _repo.UpdateCustomerAsync(customer);
+          if (!ModelState.IsValid) {
+            return BadRequest(this.ModelState);
+          }
+
+          var status = await _repo.UpdateCustomerAsync(customer);
+          if (!status) {
+            return NoContent();
+          }
+          return Ok(status);
         }
 
         // DELETE api/dataservice/customers/5
         [HttpDelete("customers/{id}")]
-        public async Task<bool> DeleteCustomer(int id)
+        public async Task<ActionResult> DeleteCustomer(int id)
         {
-          return await _repo.DeleteCustomerAsync(id);
+          var status = await _repo.DeleteCustomerAsync(id);
+          if (!status) {
+            return NotFound();
+          }
+          return Ok(status);
         }
 
         [HttpGet("states")]
-        public async Task<List<State>> States() {
-          return await _repo.GetStatesAsync();
+        public async Task<ActionResult> States() {
+          var states = await _repo.GetStatesAsync();
+          if (states == null) {
+            return NotFound();
+          }
+          return Ok(states);
         }
 
+    }
+
+    public static class HttpRequestExtensions
+    {
+        public static Uri ToUri(this HttpRequest request)
+        {
+            var hostComponents = request.Host.ToUriComponent().Split(':');
+
+            var builder = new UriBuilder
+            {
+                Scheme = request.Scheme,
+                Host = hostComponents[0],
+                Path = request.Path,
+                Query = request.QueryString.ToUriComponent()
+            };
+
+            if (hostComponents.Length == 2)
+            {
+                builder.Port = Convert.ToInt32(hostComponents[1]);
+            }
+
+            return builder.Uri;
+        }
     }
 }
